8f4ea40f85be323715a69a41eaa4dd7835150621
TotoroTron <brian.cheng.1997@gmail.com>
2024-05-17 17:42:12 -0400

    somehow less clear than c++ docs

--- cuda/ex1/scratch1.py
+++ cuda/ex1/scratch1.py
@@ -1,11 +1,12 @@
 # https://numba.pydata.org/numba-doc/latest/cuda/kernels.html
 
 from numba import jit
+from numba import cuda
 import numpy as np
 import time
 
 @cuda.jit
-def increment_by_one(array):
+def increment_v1(array):
 	# Thread id in a 1D block
 	tx = cuda.threadIdx.x
 	
@@ -19,4 +20,30 @@ def increment_by_one(array):
 	pos = tx + ty * bw
 	if pos < array.size: # Check array boundaries
 		array[pos] += 1
+	
 
+@cuda.jit
+def increment_v2(array):
+	pos = cuda.grid(1)
+	if pos < array.size:
+		array[pos] += 1
+
+def main():
+	arr = np.ones((10,), dtype=int)
+	print(arr)
+
+	a = cuda.to_device(arr)
+	c = cuda.device_array_like(a)
+	f.forall(len(a))(a, c)
+	c.copy_to_host()
+	
+	print(c)
+	
+	"""
+	print(a)
+	increment_v2(arr)
+	print(arr)
+	"""
+	
+if __name__ == '__main__':
+	main()
