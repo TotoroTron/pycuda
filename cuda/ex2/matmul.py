186eb81ea10ead9023072685d2456e4d58808c39
TotoroTron <brian.cheng.1997@gmail.com>
2024-05-20 20:36:14 -0400

    start shared mem

--- cuda/ex2/matmul.py
+++ cuda/ex2/matmul.py
@@ -57,3 +57,65 @@ def matmul_cudajit_globalmem(A, B, C):
             tmp += A[i, k] * B[k, j]
         C[i, j] = tmp
 
+@cuda.jit
+def matmul_cudajit_sharedmem(A, B, C):
+    """
+    Controls threads per block and shared memory usage.
+    The computation will be done on blocks of TPBxTPB elements.
+    
+    
+    """
+    
+    # Define an array in the shared memory.
+    # The size and type of the arrays must be known at compile time.  
+    sA = cuda.shared.array(shape=(TPB, TPB), dtype=float32)
+    sB = cuda.shared.array(shape=(TPB, TPB), dtype=float32)
+    
+    x, y = cuda.grid(2)
+
+    tx = cuda.threadIdx.x
+    ty = cuda.threadIdx.y
+    bpg = cuda.gridDim.x
+
+    
+    if x >= C.shape[0] and y >= C.shape[1]:
+        # Quit if (x, y) is outside of valid C boundary
+        return
+    
+    # Each thread computes one element in the result matrix.
+    # The dot product is chunked into dot products of TPB-long vectors.
+    tmp = 0.0
+    for i in range(bpg):
+        # Preload data into shared memory
+        sA[tx, ty] = A[x, ty + i * TPB]
+        sB[tx, ty] = B[tx + i * TPB, y]
+
+        # Wait until all threads finish preloading
+        cuda.syncthreads()
+
+        # Compute partial product on the shared memory
+        for j in range(TPB):
+            tmp += sA[tx, j] * sB[j, ty]
+
+        # Wait until all threads finish computing
+        cuda.syncthreads()
+    
+    C[x, y] = tmp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
