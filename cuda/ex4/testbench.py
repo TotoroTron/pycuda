eb50218c1c732b8d1dda13991d4a2aa3700733d8
TotoroTron <brian.cheng.1997@gmail.com>
2024-05-31 20:46:29 -0400

    fix verif/test error. still wasteful memory. not yet tested.

--- cuda/ex4/testbench.py
+++ cuda/ex4/testbench.py
@@ -8,9 +8,10 @@ import time
 class Testbench():
     def __init__(self, methods, dims):
         self._methods = methods
-        self._dims = [ (1, 1, 1) ] # (1, 1, 1) dummy test to force jit compile
+        self._dims = [ (1, 1, 1) ]  # (1, 1, 1) dummy test to force jit compile
         self._dims.extend(dims)
 
+        self._inputs = []
         self._expectations = []
         self._report = []
 
@@ -19,6 +20,7 @@ class Testbench():
             A = np.random.random(size=(dim[0], dim[2])).astype(np.float32)
             B = np.random.random(size=(dim[2], dim[1])).astype(np.float32)
             C = np.zeros(shape=(dim[0], dim[1]), dtype=np.float32)
+            self._inputs.append((A, B, C))
             self._expectations.append(mat.Numpy(A, B, C).run())
 
     def _verify(self, result, expected):
@@ -29,33 +31,23 @@ class Testbench():
     def get_results(self):
         return self._report.copy()
 
-
-    # this doesn't work.
     def test_all(self):
         print("Methods: ", self._methods)
         print("Dims: ", self._dims)
 
         self._set_expectations()
-        # Methods on outer loop:
-        # Easier indexing
-        # Have to store expectation for every dim for entire test. Wasteful memory.
-        # Verify isnt even using the same inputs as test.
-        # Have to store the inputs for every dim too. Even more waste.
-        for method in self._methods:
 
+        for method in self._methods:
             method_passfails = []
             method_times = []
 
             for idx, dim in enumerate(self._dims):
-                A = np.random.random(size=(dim[0], dim[2])).astype(np.float32)
-                B = np.random.random(size=(dim[2], dim[1])).astype(np.float32)
-                C = np.zeros(shape=(dim[0], dim[1]), dtype=np.float32)
-
-                instance = method(A, B, C) # Basic, Numpy, JitNumpy, etc.
+                A, B, C = self._inputs[idx]
+                instance = method(A, B, C)  # Basic, Numpy, JitNumpy, etc.
                 print("Instance: ", instance)
                 start_time = time.time()
                 result = instance.run()
-                elapsed_time = start_time - time.time()
+                elapsed_time = time.time() - start_time
 
                 method_passfails.append(self._verify(result, self._expectations[idx]))
                 method_times.append(elapsed_time)
@@ -71,7 +63,7 @@ class Testbench():
             self._report.append(report_entry)
 
 
-    def test_all_alt(self):
+    def _test_all_alt(self):
         print("Methods: ", self._methods)
         print("Dims: ", self._dims)
 
