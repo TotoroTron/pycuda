629a63cfbc7ec1a26206c14f9436ef3fb1e79fde
TotoroTron <brian.cheng.1997@gmail.com>
2024-06-01 16:45:25 -0400

    minor work

--- cuda/ex4/testbench.py
+++ cuda/ex4/testbench.py
@@ -63,18 +63,40 @@ class Testbench():
             self._report.append(report_entry)
 
 
-    def _test_all_alt(self):
+class Testbench_alt():
+    def __init__(self, methods, dims):
+        self._methods = methods
+        self._dims = [ (1, 1, 1) ]  # (1, 1, 1) dummy test to force jit compile
+        self._dims.extend(dims)
+
+        self._inputs = []
+        self._expectations = []
+        self._report = []
+
+    def _verify(self, result, expected):
+        print("Result:\n", result)
+        print("Expected:\n", expected)
+        return np.allclose(result, expected, rtol=1e-05, atol=1e-08)
+    
+    def get_results(self):
+        return self._report.copy()
+
+    def test_all(self):
         print("Methods: ", self._methods)
         print("Dims: ", self._dims)
-
-        self._set_expectations()
-
+        
+        for method in self._methods:
+            self._report.append( [] ) # list of empty lists
+            
+        
         for dim in self._dims:
             A = np.random.random(size=(dim[0], dim[2])).astype(np.float32)
             B = np.random.random(size=(dim[2], dim[1])).astype(np.float32)
             C = np.zeros(shape=(dim[0], dim[1]), dtype=np.float32)
-
-            #_set_expectation(A, B, C)
-
+            expectation = mat.Numpy(A, B, C).run()
+                             
             for idx, method in enumerate(self._methods):
-                pass
+                instance = method(A, B, C)
+                start_time = time.time()
+                instance.run()
+                elapsed_time = start_time - time.time()
