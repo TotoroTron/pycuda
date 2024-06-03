27fd1f2dd6a537db7e53b9dbd2f8d03cfbe46f09
TotoroTron <brian.cheng.1997@gmail.com>
2024-06-03 18:16:28 -0400

    start generalizing cudajit for non-square dot products

--- cuda/ex5/main.py
+++ cuda/ex5/main.py
@@ -13,17 +13,14 @@ def main():
     utils.print_gpu_info()
 
     methods = [ mat.Numpy, mat.CudaGlobalMemory, mat.CudaSharedMemory ]
-    squares = []
-    for i in range(1, 64+1):
-        dim = 64 * i # 64 to 4096
-        squares.append((dim, dim, dim))
+    dims = [ (256, 512, 512), (512, 256, 512), (512, 512, 256) ]
 
-    tb_squares = tb.Testbench(methods, squares)
-    tb_squares.test_all()
-    results = tb_squares.get_results()
+    test = tb.Testbench(methods, dims)
+    test.test_all()
+    results = test.get_results()
 
     utils.plot(results)
-    for idx, dim in enumerate(squares):
+    for idx, dim in enumerate(dims):
         for result in results:
             if result[1][idx] == dim:
                 print(f"Method: {result[0]:<{20}}, dims: {str(result[1][idx]):<{20}}, pass(1)/fail(0): {result[2][idx]},\t time: {result[3][idx]}")
