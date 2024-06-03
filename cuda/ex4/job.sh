0ef0d9a49bd2c13bbbd3c256a7a4ad916d94ad65
TotoroTron <brian.cheng.1997@gmail.com>
2024-06-03 17:56:21 -0400

    test on A100. cuda on TeslaK80 is actually slower than Numpy on CPU

--- cuda/ex4/job.sh
+++ cuda/ex4/job.sh
@@ -60,9 +60,11 @@ if [ "$environment" == "amarel" ]; then
 ##SBATCH --partition=main
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
+#SBATCH --exclude=cuda[001-008]
 #SBATCH --ntasks=1
 #SBATCH --mem=64000
 #SBATCH --time=01:00:00
+
 
 module purge
 module load cuda/10.0
